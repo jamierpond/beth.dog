b26ff9e948d15d0ebca17945d982d3827efc5239
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-22 18:16:10 +0000

    something

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+
 import Image from 'next/image'
 import Pup from '../public/pup_square.jpeg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center w-full">
-        <Image src={Pup} alt="Pup" className="object-cover w-64 h-64 rounded-full" />
+    // Full height of the viewport and flex container to center content
+    <main className="flex h-screen items-center justify-center">
+      <div>
+        {/* Image component from Next.js */}
+        <Image src={Pup} alt="Pup" className="object-cover object-center w-max" />
       </div>
     </main>
   );
 }
+
