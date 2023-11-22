f6cf23d2203fad042ca15d171f0299fa896e2c7f
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-22 18:04:32 +0000

    pup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import Pup from '../public/pup.jpg'
+import Pup from '../public/pup_square.jpeg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <Image src={Pup} alt="Pup" />
+      <div className="flex flex-col items-center justify-center w-full">
+        <Image src={Pup} alt="Pup" className="object-cover w-64 h-64 rounded-full" />
       </div>
     </main>
-  )
+  );
 }
